a80cee9fca0dc63ae30e533cdfeb629475f83028
laolu <femiogun9590@gmail.com>
2026-06-30 17:09:04 -0400

    Add in Login and Register Requests

---
+++ src/main/java/dev/laoluogun/mediatracker/LoginRequest.java
@@ -0,0 +1,17 @@
+package dev.laoluogun.mediatracker;
+
+public class LoginRequest {
+    
+    private String username;
+    private String password;
+
+    // Getters
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+}
---
+++ src/main/java/dev/laoluogun/mediatracker/RegisterRequest.java
@@ -0,0 +1,22 @@
+package dev.laoluogun.mediatracker;
+
+public class RegisterRequest {
+    private String username;
+    private String password;
+    private String email;
+
+    // Getters
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+}
