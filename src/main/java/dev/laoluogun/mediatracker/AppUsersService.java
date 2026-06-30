229c750325b485124409f4329a6cccd042603b13
laolu <femiogun9590@gmail.com>
2026-06-30 16:59:45 -0400

    Add Password encoder

--- src/main/java/dev/laoluogun/mediatracker/AppUsersService.java
+++ src/main/java/dev/laoluogun/mediatracker/AppUsersService.java
@@ -1,18 +1,25 @@
 package dev.laoluogun.mediatracker;
 
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 import java.util.List;
 
 @Service
 public class AppUsersService {
     
     private final AppUsersRepository appUsersRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public AppUsersService(AppUsersRepository appUsersRepository) {
+    @Autowired
+    public AppUsersService(AppUsersRepository appUsersRepository, PasswordEncoder passwordEncoder) {
         this.appUsersRepository = appUsersRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public AppUsers createAppUser(AppUsers appUser) {
+        appUser.setPassword(passwordEncoder.encode(appUser.getPassword()));
         return appUsersRepository.save(appUser);
     }
 
