475c92c197518e2bcc1ffaa40934f15ac0921e16
laolu <femiogun9590@gmail.com>
2026-06-30 17:00:35 -0400

    Add JWTUtil class

---
+++ src/main/java/dev/laoluogun/mediatracker/JwTUtil.java
@@ -0,0 +1,53 @@
+package dev.laoluogun.mediatracker;
+
+import org.springframework.stereotype.Component;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+
+import java.util.Date;
+import javax.crypto.SecretKey;
+
+@Component
+public class JwTUtil {
+    
+    private final String SECRET_KEY = "your-256-bit-secret-your-256-bit-secret"; 
+    private final long EXPIRATION_MS = 86400000; // 1 day in milliseconds
+
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    }
+
+    public String generateToken(String username) {
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + EXPIRATION_MS);
+
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(now)
+                .expiration(expiryDate)
+                .signWith(getSigningKey()).compact();
+    }
+
+    public String extractUsername(String token) {
+        return getClaims(token).getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            getClaims(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public Claims getClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+}
